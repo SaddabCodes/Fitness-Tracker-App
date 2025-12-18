04c7398772433a29356ed0b775f29d3138b9de4f
saddab <saddabakhtar17@gmail.com>
2025-12-18 23:16:05 +0530

    create fintess tracker

---
+++ src/main/java/com/sadcodes/fitnesstrackerapp/FitnessTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.sadcodes.fitnesstrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessTrackerAppApplication.class, args);
+    }
+
+}
