78de321934040127160d0bc6cb8f42fa42ebf383
saddab <saddabakhtar17@gmail.com>
2025-12-19 15:04:59 +0530

    update controller and service class

--- src/main/java/com/sadcodes/fitnesstrackerapp/controller/AuthController.java
+++ src/main/java/com/sadcodes/fitnesstrackerapp/controller/AuthController.java
@@ -1,7 +1,24 @@
 package com.sadcodes.fitnesstrackerapp.controller;
 
+import com.sadcodes.fitnesstrackerapp.model.User;
+import com.sadcodes.fitnesstrackerapp.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return userService.register(user);
+    }
 }
--- src/main/java/com/sadcodes/fitnesstrackerapp/service/UserService.java
+++ src/main/java/com/sadcodes/fitnesstrackerapp/service/UserService.java
@@ -1,7 +1,19 @@
 package com.sadcodes.fitnesstrackerapp.service;
 
+import com.sadcodes.fitnesstrackerapp.model.User;
+import com.sadcodes.fitnesstrackerapp.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User register(User user) {
+        return userRepository.save(user);
+    }
 }
