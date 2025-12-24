d7a89bd78f682427d02a77a265a76c839afced5d
saddab <saddabakhtar17@gmail.com>
2025-12-24 13:10:17 +0530

    complete validation and add exception hanlder

--- src/main/java/com/sadcodes/fitnesstrackerapp/controller/AuthController.java
+++ src/main/java/com/sadcodes/fitnesstrackerapp/controller/AuthController.java
@@ -4,15 +4,13 @@ import com.sadcodes.fitnesstrackerapp.dto.LoginRequest;
 import com.sadcodes.fitnesstrackerapp.dto.LoginResponse;
 import com.sadcodes.fitnesstrackerapp.dto.RegisterRequest;
 import com.sadcodes.fitnesstrackerapp.dto.UserResponse;
-import com.sadcodes.fitnesstrackerapp.model.User;
 import com.sadcodes.fitnesstrackerapp.repository.UserRepository;
 import com.sadcodes.fitnesstrackerapp.security.JwtUtils;
 import com.sadcodes.fitnesstrackerapp.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +29,7 @@ public class AuthController {
 
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest registerRequest) {
+    public ResponseEntity<UserResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
         return new ResponseEntity<>(userService.register(registerRequest), HttpStatus.CREATED);
     }
 
--- src/main/java/com/sadcodes/fitnesstrackerapp/dto/RegisterRequest.java
+++ src/main/java/com/sadcodes/fitnesstrackerapp/dto/RegisterRequest.java
@@ -1,6 +1,8 @@
 package com.sadcodes.fitnesstrackerapp.dto;
 
 import com.sadcodes.fitnesstrackerapp.model.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,12 @@ import lombok.NoArgsConstructor;
 public class RegisterRequest {
     private String firstName;
     private String lastName;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @NotBlank(message = "Email is required")
+    @Email
     private String email;
     private UserRole role;
 }
